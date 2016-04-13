19cee6d3aa6e6dd6d0afc78dad6d935081c21f32
shawngrant333 <shawngrant333@gmail.com>
2016-04-13 08:43:20 -0500

    content added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
-if($(window).scrollTop()>20){
-	$("#head-name").fadeTo(1000,0).delay(1000).css("display","none");
-}else{
-	$("#head-name").css("display","block").fadeTo(1000,100);
+while (true)
+{
+	if ($(window).scrollTop() > 20)
+	{
+		$('#head-name').fadeTo(500, 0).delay(500).css("display", "none");
+	}
+	else
+	{
+		$('#head-name').css("display", "block").fadeTo(500, 100);
+	}
 }
 
 function scrollTo(id) {
 	$('html, body').animate({ 
-		scrollTop: $(id).offset().top
-		}, 'fast');
-	}
-
+								scrollTop: $(id).offset().top
+							}, 'fast');
+}
 
