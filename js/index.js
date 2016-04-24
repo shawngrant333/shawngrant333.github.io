049834239ffc8e328349a7c52964c9ea0b63223e
shawngrant333 <shawngrant333@gmail.com>
2016-04-23 22:43:22 -0500

    hobbies dialogs setup

--- js/index.js
+++ js/index.js
@@ -16,3 +16,16 @@ function scrollTo(id) {
 							}, 'slow');
 }
 
+function HobbyAlert(id){
+	if(id=="drawing"){
+		alert("Drawing is one of my many talents.\n You could say I have gifted hands");
+	}else if(id=="music"){
+		alert("I listen to music while doing most of my daily activities");
+	}else if(id=="code"){
+		alert("Programming is my superpower, I spend 80% of my time coding");
+	}else if(id=="web"){
+		alert("I'm surfing the web even when I have no reson to");
+	}else if(id=="tv"){
+		alert("When a computer isn't available you'll find me watching some good ole television");
+	}
+}
