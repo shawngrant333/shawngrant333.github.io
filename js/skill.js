a0c85da87b462fe244c20d7c0235325b69d46dbb
shawn-grant <shawngrant333@gmail.com>
2016-07-09 20:26:15 -0500

    fix colors

--- js/skill.js
+++ js/skill.js
@@ -8,25 +8,25 @@ function setWebArc() {
 	var ctx = C.getContext("2d");
 	ctx.lineWidth = "20";
 //For Html
-	ctx.strokeStyle = "C95411";
+	ctx.strokeStyle = "#C95411";
 	ctx.beginPath();
 	ctx.arc(150, 150, 120, degToRad(270), degToRad(220));
 	ctx.stroke(); 
 
 //For CSS
-	ctx.strokeStyle = "142DDF";
+	ctx.strokeStyle = "#142DDF";
 	ctx.beginPath();
 	ctx.arc(150, 150, 100, degToRad(270), degToRad(200));
 	ctx.stroke();
 
 //For Javascript
-	ctx.strokeStyle = "E7DD14";
+	ctx.strokeStyle = "#E7DD14";
 	ctx.beginPath();
 	ctx.arc(150, 150, 80, degToRad(270), degToRad(150));
 	ctx.stroke();
 
 //For PHP
-	ctx.strokeStyle = "5A0076";
+	ctx.strokeStyle = "#5A0076";
 	ctx.beginPath();
 	ctx.arc(150, 150, 60, degToRad(270), degToRad(120));
 	ctx.stroke();
@@ -37,19 +37,19 @@ function setMobileArc() {
 	var ctx = C.getContext("2d");
 	ctx.lineWidth = "20";
 //For JAVA
-	ctx.strokeStyle = "69E7D0";
+	ctx.strokeStyle = "#69E7D0";
 	ctx.beginPath();
 	ctx.arc(150, 150, 120, degToRad(270), degToRad(200));
 	ctx.stroke();
 
 //For XML
-	ctx.strokeStyle = "ff0f00";
+	ctx.strokeStyle = "#ff0f00";
 	ctx.beginPath();
 	ctx.arc(150, 150, 100, degToRad(270), degToRad(230));
 	ctx.stroke();
 
 //For UNITY
-	ctx.strokeStyle = "000000";
+	ctx.strokeStyle = "#000000";
 	ctx.beginPath();
 	ctx.arc(150, 150, 80, degToRad(270), degToRad(100));
 	ctx.stroke();
@@ -60,19 +60,19 @@ function setStandardArc() {
 	var ctx = C.getContext("2d");
 	ctx.lineWidth = "20";
 //For C
-	ctx.strokeStyle = "11FC00";
+	ctx.strokeStyle = "#11FC00";
 	ctx.beginPath();
 	ctx.arc(150, 150, 120, degToRad(270), degToRad(100));
 	ctx.stroke();
 
 //For C++
-	ctx.strokeStyle = "ffffff";
+	ctx.strokeStyle = "#ffffff";
 	ctx.beginPath();
 	ctx.arc(150, 150, 100, degToRad(270), degToRad(80));
 	ctx.stroke();
 
 //For C#
-	ctx.strokeStyle = "E4C08F";
+	ctx.strokeStyle = "#E4C08F";
 	ctx.beginPath();
 	ctx.arc(150, 150, 80, degToRad(270), degToRad(150));
 	ctx.stroke();
