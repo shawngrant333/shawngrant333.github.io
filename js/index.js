24d93eceaf35fb96739e63cca333bea374e9884d
Shawn Grant <shawngrant333@gmail.com>
2017-03-11 20:13:01 -0500

    renovate

--- js/index.js
+++ js/index.js
@@ -14,7 +14,6 @@ function ClosePortfolio(){
 																												"-moz-animation:slideOutDown 1s;"+
 																											  "-webkit-animation:slideOutDown 1s;"+
 																											  "animation:slideOutDown 1s;";
-
 	setTimeout(function(){
 		document.getElementsByClassName('portfolio')[0].style="display:none;";
    }, 1000);
@@ -32,4 +31,22 @@ function HobbyAlert(id){
 	}else if(id=="tv"){
 		alert("You can find me watching some good ol' TV");
 	}
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
