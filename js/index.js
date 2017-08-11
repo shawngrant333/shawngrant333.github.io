815979f262e31c306a1485c73f75f81003cb0113
Shawn Grant <shawngrant333@gmail.com>
2017-08-10 21:29:19 -0400

    second update

--- js/index.js
+++ js/index.js
@@ -19,20 +19,6 @@ function ClosePortfolio(){
    }, 1000);
 }
 
-function HobbyAlert(id){
-	if(id=="drawing"){
-		alert("Drawing is one of my many talents.\n You could say I have gifted hands");
-	}else if(id=="music"){
-		alert("I listen to music while doing most of my daily activities");
-	}else if(id=="code"){
-		alert("Programming is my superpower, I spend 80% of my time coding");
-	}else if(id=="web"){
-		alert("I'm always surfing the web");
-	}else if(id=="tv"){
-		alert("You can find me watching some good ol' TV");
-	}
-}
-
 var slideIndex = 1;
 showDivs(slideIndex);
 
--- js/skill.js
+++ js/skill.js
@@ -1,83 +1,14 @@
-function degToRad(degree) {
-	var factor = Math.PI / 180;
-	return degree * factor;
+var canvases = document.getElementsByClassName("Skill");
+
+for (var i = 0; i < canvases.length; i++){
+	var canvas = canvases[i];
+	var name = canvas.className.split(' ')[1];
+	var color = canvas.className.split(' ')[2];
+
+	var ctx = canvas.getContext("2d");
+	ctx.font = "20px arial";
+	ctx.fillText(name,5,25);
+
+	ctx.fillStyle = color;
+	ctx.fillRect(canvas.width - 20,0,20,30);
 }
-
-function setWebArc() {
-	var C = document.getElementById("webCanvas");
-	var ctx = C.getContext("2d");
-	ctx.lineWidth = "20";
-//For Html
-	ctx.strokeStyle = "#C95411";
-	ctx.beginPath();
-	ctx.arc(150, 150, 120, degToRad(270), degToRad(220));
-	ctx.stroke();
-
-//For CSS
-	ctx.strokeStyle = "#142DDF";
-	ctx.beginPath();
-	ctx.arc(150, 150, 100, degToRad(270), degToRad(200));
-	ctx.stroke();
-
-//For Javascript
-	ctx.strokeStyle = "#E7DD14";
-	ctx.beginPath();
-	ctx.arc(150, 150, 80, degToRad(270), degToRad(150));
-	ctx.stroke();
-
-//For PHP
-	ctx.strokeStyle = "#5A0076";
-	ctx.beginPath();
-	ctx.arc(150, 150, 60, degToRad(270), degToRad(120));
-	ctx.stroke();
-}
-
-function setMobileArc() {
-	var C = document.getElementById("mobileCanvas");
-	var ctx = C.getContext("2d");
-	ctx.lineWidth = "20";
-//For JAVA
-	ctx.strokeStyle = "#69E7D0";
-	ctx.beginPath();
-	ctx.arc(150, 150, 120, degToRad(270), degToRad(200));
-	ctx.stroke();
-
-//For XML
-	ctx.strokeStyle = "#ff0f00";
-	ctx.beginPath();
-	ctx.arc(150, 150, 100, degToRad(270), degToRad(230));
-	ctx.stroke();
-
-//For UNITY
-	ctx.strokeStyle = "#000000";
-	ctx.beginPath();
-	ctx.arc(150, 150, 80, degToRad(270), degToRad(100));
-	ctx.stroke();
-}
-
-function setStandardArc() {
-	var C = document.getElementById("otherCanvas");
-	var ctx = C.getContext("2d");
-	ctx.lineWidth = "20";
-//For C
-	ctx.strokeStyle = "#11FC00";
-	ctx.beginPath();
-	ctx.arc(150, 150, 120, degToRad(270), degToRad(100));
-	ctx.stroke();
-
-//For C++
-	ctx.strokeStyle = "#ff7155";
-	ctx.beginPath();
-	ctx.arc(150, 150, 100, degToRad(270), degToRad(80));
-	ctx.stroke();
-
-//For C#
-	ctx.strokeStyle = "#E4C08F";
-	ctx.beginPath();
-	ctx.arc(150, 150, 80, degToRad(270), degToRad(150));
-	ctx.stroke();
-}
-
-setWebArc();
-setMobileArc();
-setStandardArc();
