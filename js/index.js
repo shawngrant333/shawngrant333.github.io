3d3abc28d74c4bda43ee7284ffa3cb7bba353d69
Shawn Grant <shawngrant333@gmail.com>
2020-08-04 23:06:54 -0500

    minor update

--- js/index.js
+++ js/index.js
@@ -5,26 +5,30 @@ function scrollToElement(id) {
 }
 
 function openTab(evt, tabName) {
+
 	var i, tabcontent, tablinks;
 	tabcontent = document.getElementsByClassName("tabcontent");
-
-	for (i = 0; i < tabcontent.length; i++) {
-	  tabcontent[i].style.display = "none";
-	}
-
 	tablinks = document.getElementsByClassName("tablinks");
 
-	for (i = 0; i < tablinks.length; i++) {
-	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	//
+
+
+	if (evt.currentTarget.className == "tablinks active") { //already active
+		evt.currentTarget.className = "tablinks"
+		$("#"+tabName).slideUp("slow");
+	} else { //make active
+		for (i = 0; i < tabcontent.length; i++) {
+			tabcontent[i].style.display = "none";
+		}
+		//document.getElementById(tabName).style.display = "block";
+
+		for (i = 0; i < tablinks.length; i++) {
+			tablinks[i].className = "tablinks";
+		}
+		evt.currentTarget.className += " active";
+
+		$("#"+tabName).slideDown("slow");
 	}
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
-
-	scrollToElement("#tab");
+	scrollToElement("#intro-social");
 }
-
-function closeTab(tab){
-	
-}
-
 //document.getElementById("defaultTab").click();
